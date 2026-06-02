d10c196bbf13d3c34d49dee986e0d5c3fa7400c7
MarcosVinic1us <marcosvinicius007834@gmail.com>
2026-06-02 14:37:16 -0300

    Add files via upload

---
+++ UC31 01 06/templates/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/cadrastro', methods =['GET','POST'])
+def cadrastro():
+
+    mnesagem = ""
+
+    if request.method == 'POST':
+        if not nome:
+            mensagem = "O campo nome é obrigatório"
+    elif:
+        mensagem = f"Cadrastrom realizado com sucesso! Bem-vindo, {nome}"
+        
+    return render_template('cadrastro.html', mensagem=mensagem)
+
+@app.route('/validacao', methods = ['POST'])
+def cadrastro():
+
+    name = request.form.get('nome', '').strip().tittle()
+    email = request.form.get('email', '').strip().lower()
+    cidade = request.form.get('cidade', '').strip().tittle()
+
+    return f"""
+    Nome: {nome}<br>
+    Email: {email}<br>
+    Cidade: {cidade}
+    """
+if__name__ == '__main__':
+    app.run(debug =True)
+
